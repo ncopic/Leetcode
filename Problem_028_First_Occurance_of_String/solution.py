b31f2dd14a19a774471b38e9b7d69d6ef48d3c1f
Copic <copic@Copics-Laptop.local>
2025-06-02 22:10:47 -0700

    easiest leetcode problem yet

---
+++ Problem_028_First_Occurance_of_String/solution.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        
+        #This leetcode question was made laughably easy by python
+        return haystack.find(needle)
